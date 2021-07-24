0d357a4867ff01ba65a17368422b59f3ed9b000e
Sri Subathra Devi B <subathrad18@gmail.com>
2021-07-24 19:34:06 +0530

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -6,3 +6,10 @@ def connect():
     cur.execute("CREATE TABLE IF NOT EXISTS contact (id INTEGER NOT NULL  PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, gender TEXT, address TEXT, contact INTEGER)")
     con.commit()
     con.close()
+    
+def insert(first_name,last_name,gender,address,contact):
+    con=sqlite3.connect("contacts.db")
+    cur=con.cursor()
+    cur.execute("INSERT INTO contact VALUES(NULL,?,?,?,?,?)",(first_name,last_name,gender,address,contact))
+    con.commit()
+    con.close()
