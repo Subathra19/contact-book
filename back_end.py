0a27b0b2ddb7da8f5e574744f27a2263ab6ec86e
Sri Subathra Devi B <subathrad18@gmail.com>
2021-07-24 19:34:39 +0530

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -13,3 +13,21 @@ def insert(first_name,last_name,gender,address,contact):
     cur.execute("INSERT INTO contact VALUES(NULL,?,?,?,?,?)",(first_name,last_name,gender,address,contact))
     con.commit()
     con.close()
+    
+def view():
+    con=sqlite3.connect("contacts.db")
+    cur=con.cursor()
+    cur.execute("SELECT * FROM contact")
+    data=cur.fetchall()
+    con.commit()
+    con.close()    
+    return data
+
+def search(first_name="",last_name="",gender="",address="",contact=""):
+    con=sqlite3.connect("contacts.db")
+    cur=con.cursor()
+    cur.execute("SELECT * FROM contact WHERE firstname=? or lastname=? or gender=? or address=? or contact=?",(first_name,last_name,gender,address,contact))
+    data=cur.fetchall()
+    con.commit()
+    con.close()    
+    return data
